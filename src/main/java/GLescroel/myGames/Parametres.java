b687b84ec1f15935854e3343c6fab224473c12ab
GLescroel <gregory.lescroel@axa.fr>
2019-03-04 17:50:51 +0100

    suppression import inutile

--- src/main/java/GLescroel/myGames/Parametres.java
+++ src/main/java/GLescroel/myGames/Parametres.java
@@ -2,7 +2,6 @@ package GLescroel.myGames;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
